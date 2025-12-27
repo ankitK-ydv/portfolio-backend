4e361ebb5ee65973c93924c7cf03300d01820ade
ankitK-ydv <ankit8288ya@gmail.com>
2025-12-27 19:35:36 +0530

    Backend for portfolio contact form

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post("/contact", async (req, res) => {
+  const { name, email, message } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS
+    }
+  });
+
+  const mailOptions = {
+    from: email,
+    to: process.env.EMAIL_USER,
+    subject: "New Message from Portfolio Website",
+    text: `
+You have a new message from client on portfolio page.
+
+Name: ${name}
+Email: ${email}
+Message: ${message}
+    `
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    res.json({ success: true, message: "Message sent successfully" });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Email failed" });
+  }
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
