e15d2938394bf8902a3669a2cbd1bbf0e03edae7
ankitK-ydv <ankit8288ya@gmail.com>
2025-12-27 21:01:18 +0530

    Fix port for Render deployment

--- server.js
+++ server.js
@@ -39,6 +39,7 @@ Message: ${message}
   }
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server running");
 });
